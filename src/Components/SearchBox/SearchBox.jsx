ec40f15c8772ef7d07153fddf64ca68cf4843c85
mittalaakash <mit.ash.619@gmail.com>
2021-08-12 14:42:32 +0530

    useref

--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -1,18 +1,23 @@
-import { React } from 'react';
+import React, { useRef, useEffect } from 'react';
 import './SearchBox.css';
 
 const SearchBox = ({ inputChange }) => {
-    return (
-        <div className="search-container">
-            <input
-                onChange={(event) => {
-                    inputChange(event.target.value);
-                }}
-                placeholder="Type Keyword"
-                className="search-input"
-            />
-        </div>
-    );
+  const ref = useRef(null);
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+  return (
+    <div className='search-container'>
+      <input
+        ref={ref}
+        onChange={event => {
+          inputChange(event.target.value);
+        }}
+        placeholder='Type Keyword'
+        className='search-input'
+      />
+    </div>
+  );
 };
 
 export default SearchBox;
